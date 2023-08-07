d5ea07279400e36fb7de54d9ae5780bf0b1d3c9c
SurajSinh Rathod <surajsinhrathod75@gmail.com>
2023-08-07 13:10:00 +0530

    Navigation added !

--- app/src/main/java/com/surajrathod/bottomnavdemo/HomeActivity.kt
+++ app/src/main/java/com/surajrathod/bottomnavdemo/HomeActivity.kt
@@ -2,10 +2,25 @@ package com.surajrathod.bottomnavdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.surajrathod.bottomnavdemo.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomHome.setupWithNavController(navController)
     }
 }
